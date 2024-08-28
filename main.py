0356d5cd4e7300598e7ff8018e8c282271ad8c48
Fardin Alam <fardinalam4@gmail.com>
2024-08-28 21:49:05 +0600

    Second commit for PostgreSQL Integration (added get_db connection & POST request)

--- main.py
+++ main.py
@@ -1,19 +1,17 @@
-from fastapi import FastAPI, HTTPException
+from fastapi import FastAPI, HTTPException, Depends
 from pydantic import BaseModel
 from sqlalchemy import create_engine, Column, Integer, String, Float
-from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm import sessionmaker, Session
 from sqlalchemy.ext.declarative import declarative_base
 
 DATABASE_URL = "postgresql://postgres:postgres@localhost/FastAPI_db"
 
 engine = create_engine(DATABASE_URL)
 
-SessionLocal = sessionmaker(automatic=False, autoflush=False, bind=engine)
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
 
 app = FastAPI()
-
-items = []
 
 
 class Item(Base):
@@ -23,3 +21,28 @@ class Item(Base):
     name = Column(String, index=True)
     description = Column(String, index=True)
     price = Column(Integer, index=True)
+
+Base.metadata.create_all(bind=engine)
+
+
+class ItemCreate(BaseModel):
+    name: str
+    description: str = None
+    price: float
+
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
+
+@app.post("/items")
+def create_item(item: ItemCreate, db: Session = Depends(get_db)):
+    db_item = Item(name=item.name, description=item.description, price=item.price)
+    db.add(db_item)
+    db.commit()
+    db.refresh(db_item)
+    return db_item
