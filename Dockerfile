e14229dd27212deb73e631f8aff9c6a08d0201b4
Fardin Alam <fardinalam4@gmail.com>
2024-08-29 15:27:24 +0600

    Final commit after successfully integrating PostreSQL with Docker

--- Dockerfile
+++ Dockerfile
@@ -4,6 +4,8 @@ FROM python:3.10-slim
 
 WORKDIR /app
 
+RUN apt-get update && apt-get install -y gcc python3-dev libpq-dev
+
 # Copy and install dependencies.
 COPY requirements.txt .
 
@@ -12,4 +14,4 @@ RUN pip install --no-cache-dir -r requirements.txt
 #Copy the application code
 COPY . .
 
-CMD ["uvicorn", "main:app", "--host", "0.0.0.0", "--port", "8000"]
+
--- main.py
+++ main.py
@@ -5,8 +5,9 @@ from sqlalchemy.orm import sessionmaker, Session
 from sqlalchemy.ext.declarative import declarative_base
 
 # Specifying connection details to the PostgreSQL DB.
-DATABASE_URL = "postgresql://postgres:postgres@localhost/FastAPI_db"
+DATABASE_URL = "postgresql://postgres:postgres@db:5432/FastAPI_db"
 
+# db:5432 - localhost
 engine = create_engine(DATABASE_URL)
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
